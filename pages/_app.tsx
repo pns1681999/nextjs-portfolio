c9f4426c8b0ac4413e24e6ca90047efc1e6b3c41
pnson <43614413+pns1681999@users.noreply.github.com>
2022-07-23 11:29:26 +0700

    feat: themes manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,17 @@ import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
 import '../styles/themes.scss';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (localStorage.getItem("theme")) {
+      document.documentElement.setAttribute(
+        "data-theme",
+        localStorage.getItem("theme")
+      );
+    }
+  }, []);
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,12 +1,73 @@
 import type { NextPage } from 'next'
 import Head from '../components/Head'
+import ThemeInfo from '../components/ThemeInfo'
+import styles from '../styles/Setting.module.scss'
 const Settings: NextPage = () => {
+  const themes = [
+    {
+      name: "GitHub Dark",
+      icon: "/github-dark.png",
+      publisher: "GitHub",
+      theme: "github-dark",
+      description: "GitHub theme for VS Code"
+    },
+    {
+      name: "Dracula",
+      icon: "/dracula.png",
+      publisher: "Dracula Theme",
+      theme: "dracula",
+      description: "Official Dracula Theme. A dark theme for many editors, shells, and more."
+    },
+    {
+      name: "Ayu Dark",
+      icon: "/ayu.png",
+      publisher: "teabyii",
+      theme: "ayu-dark",
+      description: "A simple theme with bright colors."
+    },
+    {
+      name: "Ayu Mirage",
+      icon: "/ayu.png",
+      publisher: "teabyii",
+      theme: "ayu-mirage",
+      description: "A simple theme with bright colors."
+    },
+    {
+      name: "Nord",
+      icon: "/nord.png",
+      publisher: "arcticicestudio",
+      theme: "nord",
+      description: "An arctic, north-bluish clean and elegant Visual Studio Code theme."
+    },
+    {
+      name: "Night Owl",
+      icon: "/night-owl.png",
+      publisher: "sarah.drasner",
+      theme: "night-owl",
+      description: "A VS Code theme for the night owls out there."
+    },
+  ]
   return (
     <>
       <Head title="Son Pham | Settings"></Head>
       <h1>
-        Settings page
+        Manager themes
       </h1>
+      <div className={styles.container}>
+        {
+          themes.map((theme) => {
+            
+            return (<ThemeInfo
+            key={theme.name}
+            name={theme.name}
+            icon={theme.icon}
+            publisher={theme.publisher}
+            theme={theme.theme}
+          />)
+          })
+        }
+      </div>
+      
     </>
   )
 }
